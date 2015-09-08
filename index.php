296813ba6860db003a1375afeb2db796f9626da3
oshiimizunohuta <g.ebodai@gmail.com>
2015-09-08 15:02:56 +0900

    googleAnalyticsに交換

--- index.php
+++ index.php
@@ -38,19 +38,24 @@
 		<script src="./litrosound/Litrosound.js"></script>
 		<script src="./LitroReceiver.js"></script>
 
-		<!-- AWS Analytics SDK -->
-		<script src="/libjs/aws-sdk.min.js"></script>
-		<script src="/libjs/aws-sdk-mobile-analytics.min.js"></script>
-		<script type="text/javascript">
-			var options = {
-				appId : '284cad9a703d47589109f9cb12038e22', //Amazon Mobile Analytics App ID
-				appTitle : LITRORECEIVER_NAME, //Optional e.g. 'Example App'
-				// appVersionName : APP_VERSION_NAME, //Optional e.g. '1.4.1'
-				appVersionCode : LITRORECEIVER_VERSION, //Optional e.g. '42'
-				// appPackageName : APP_PACKAGE_NAME //Optional e.g. 'com.amazon.example'
-			};
+		<!-- GoogleAnalytics -->
+		<script>
+			(function(i, s, o, g, r, a, m) {
+				i['GoogleAnalyticsObject'] = r;
+				i[r] = i[r] ||
+				function() {
+					(i[r].q = i[r].q || []).push(arguments)
+				}, i[r].l = 1 * new Date();
+				a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+				a.async = 1;
+				a.src = g;
+				m.parentNode.insertBefore(a, m)
+			})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-			var mobileAnalyticsClient = new AMA.Manager(options);
+			ga('create', 'UA-46774451-3', 'auto');
+			ga('send', 'pageview');
+
 		</script>
 		<style>
 			
